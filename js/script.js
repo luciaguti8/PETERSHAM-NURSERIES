57547fcc458fe7fca3f67a14b32287925faba0b5
luciaguti8 <luciaguti4design@gmail.com>
2025-01-02 22:07:14 +0100

    nav-footer

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    document.querySelectorAll('.nav-link').forEach(link => {
+        link.addEventListener('click', () => {
+            document.querySelectorAll('.nav-link').forEach(item => item.classList.remove('active'));
+            link.classList.add('active');
+        });
+    });
+});
