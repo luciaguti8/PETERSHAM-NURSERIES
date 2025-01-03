eeeee87b5a29e207dadfa39049af61fe4eacf82f
luciaguti8 <luciaguti4design@gmail.com>
2025-01-03 23:10:40 +0100

    header-intro-story

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+    var scroll = new LocomotiveScroll({
+        el: document.querySelector('[data-scroll-container]'),
+        smooth: true
+    });
+
     document.querySelectorAll('.nav-link').forEach(link => {
         link.addEventListener('click', () => {
             document.querySelectorAll('.nav-link').forEach(item => item.classList.remove('active'));
